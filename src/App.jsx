d3a1aab0cfc1df48aefbb163131549a43f75e0e7
lihoubrak <blh.braklihou18@gmail.com>
2024-12-25 01:17:21 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -279,7 +279,6 @@ const App = () => {
         highlightOnHover
         responsive
       />
-
       <Modal
         isOpen={isModalOpen}
         onRequestClose={() => setIsModalOpen(false)}
@@ -297,31 +296,32 @@ const App = () => {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            overflow: "hidden",
+            overflow: "hidden", // Ensure overflow is hidden for the background
             backgroundColor: "#000",
           },
           overlay: { backgroundColor: "rgba(0, 0, 0, 0.9)" },
         }}
       >
-        <div>
+        <div className="relative w-full h-full">
           <button
             onClick={() => setIsModalOpen(false)}
-            className="absolute top-2 right-2 text-white bg-red-500 rounded-full p-2 hover:bg-red-600"
+            className="absolute top-2 right-2 text-white bg-red-500 rounded-full p-2 hover:bg-red-600 z-10"
           >
             <FaTimes />
           </button>
-          <div className="flex flex-wrap justify-center gap-4 p-4">
+          <div className="flex flex-wrap justify-center gap-4 p-4 overflow-y-auto w-full h-full">
             {selectedImages.length > 0 ? (
               selectedImages.map((url, idx) => (
-                <img
-                  key={idx}
-                  src={`https://drive.google.com/thumbnail?id=${
-                    url.split("id=")[1]
-                  }`}
-                  alt={`Thumbnail ${idx + 1}`}
-                  onClick={() => window.open(url, "_blank")}
-                  className="cursor-pointer border border-gray-300 rounded shadow-md"
-                />
+                <div key={idx} className="w-1/3 p-2">
+                  <img
+                    src={`https://drive.google.com/thumbnail?id=${
+                      url.split("id=")[1]
+                    }`}
+                    alt={`Thumbnail ${idx + 1}`}
+                    onClick={() => window.open(url, "_blank")}
+                    className="cursor-pointer border border-gray-300 rounded shadow-md w-full h-auto object-contain"
+                  />
+                </div>
               ))
             ) : (
               <p className="text-center text-gray-500">No images available</p>
